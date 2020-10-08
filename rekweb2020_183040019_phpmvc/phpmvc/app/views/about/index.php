62e2e26d27896f32335681153ff6e177ff60337f
Arfahmaulidya <71435655+Arfahmaulidya@users.noreply.github.com>
2020-10-08 20:56:19 +0700

    Add files via upload

---
+++ rekweb2020_183040019_phpmvc/phpmvc/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/arfah.jpg" alt="Arfah Maulidya" width="200" class="rounded-shadow">
+	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah <?= $data['pekerjaan']; ?></p>
+</div>
